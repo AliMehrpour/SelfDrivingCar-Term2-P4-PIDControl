6344eecfac36f4b567804057b620480f704470c7
Ali Mehrpour <Alimehrpour@gmail.com>
2017-07-01 02:10:18 -0700

    Implement PID Control

---
+++ src/PID.cpp
@@ -0,0 +1,26 @@
+#include "PID.h"
+#include <iostream>
+
+using namespace std;
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+
+  p_error = d_error = i_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+}
+
+double PID::TotalError() {
+  return p_error + i_error + d_error;
+}
